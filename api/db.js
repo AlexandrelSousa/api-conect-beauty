888241b756fe221ed96ad54d51e0948cd13b6a51
Alexandrelsousa <alexandrelsousa@outlook.com>
2024-11-05 11:29:48 -0200

    retirando conexão com o banco

--- api/db.js
+++ api/db.js
@@ -1,24 +1,5 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
-    connectionTimeoutMillis: 10000 // 10 segundos
-  });
-  
-
-// Testando a conexão
-(async () => {
-    try {
-        const client = await pool.connect();
-        console.log("Conexão bem-sucedida ao banco de dados!");
-        client.release(); // Libera o cliente após a conexão
-    } catch (err) {
-        console.error("Erro ao conectar ao banco de dados:", err);
-    }
-})();
-
-pool.connect();
 
 module.exports = pool;

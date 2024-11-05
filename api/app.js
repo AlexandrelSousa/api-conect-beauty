73e74989f3915877f239688de03f9deb33df7cfb
Alexandrelsousa <alexandrelsousa@outlook.com>
2024-11-05 18:10:06 -0200

    adicionando a rota de login

--- api/app.js
+++ api/app.js
@@ -1,37 +1,21 @@
 // /api/app.js
 const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const cors = require('./middleware/cors');
 const upload = require('./config/multer');
 const pool = require('./db');
+const authRoutes = require('./routes/auth'); // Importando as rotas de autenticação
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-// CORS Middleware
-//app.use((req, res, next) => cors(req, res, next));
-
 // Test Route
 app.get('/', (req, res) => res.send('API is running'));
 
-// Sample User Route
-app.post('/upload', upload.single('file'), (req, res) => {
-    res.send({ message: 'File uploaded successfully' });
-});
-
-// Pool Connection Example
-app.get('/users', async (req, res) => {
-    try {
-        const result = await pool.query('SELECT * FROM users');
-        res.json(result.rows);
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao buscar usuários' });
-    }
-});
+// Rota de autenticação
+app.use('/api', authRoutes); // Usando as rotas de autenticação
 
 // Configurar o servidor para ouvir em uma porta
 const PORT = 3030;
---
+++ api/routes/auth.js
@@ -0,0 +1,44 @@
+// /api/routes/auth.js
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const pool = require('../db');
+
+const router = express.Router();
+
+router.post('/login', async (req, res) => {
+    const { emailOuCNPJ, senha } = req.body;
+
+    try {
+        const user = await pool.query('SELECT * FROM cliente WHERE email = $1', [emailOuCNPJ]);
+        const cnpjUser = await pool.query('SELECT * FROM empreendedora WHERE cnpj = $1', [emailOuCNPJ]);
+
+        if (user.rows.length === 0 && cnpjUser.rows.length === 0) {
+            return res.status(401).json({ message: 'Dados de login incorretos.' });
+        }
+
+        let validPassword;
+        let acessToken;
+
+        if (cnpjUser.rows.length > 0) {
+            validPassword = await bcrypt.compare(senha, cnpjUser.rows[0].senha);
+            if (!validPassword) {
+                return res.status(401).json({ message: 'Nome de usuário ou senha incorretos' });
+            }
+            acessToken = jwt.sign({ username: cnpjUser.rows[0].nome, cnpj: cnpjUser.rows[0].cnpj }, process.env.ACESS_TOKEN_SECRET);
+        } else {
+            validPassword = await bcrypt.compare(senha, user.rows[0].senha);
+            if (!validPassword) {
+                return res.status(401).json({ message: 'Nome de usuário ou senha incorretos' });
+            }
+            acessToken = jwt.sign({ username: user.rows[0].nome, id: user.rows[0].id }, process.env.ACESS_TOKEN_SECRET);
+        }
+
+        res.json({ acessToken });
+    } catch (error) {
+        console.error('Erro ao processar login:', error);
+        res.status(500).json({ message: 'Erro interno do servidor' });
+    }
+});
+
+module.exports = router;

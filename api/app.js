caa709d104b722d2aaf9d183fa02c1055269e499
Alexandrelsousa <alexandrelsousa@outlook.com>
2024-11-05 18:03:06 -0200

    bcrypt é vc?

--- api/app.js
+++ api/app.js
@@ -1,6 +1,6 @@
 // /api/app.js
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const cors = require('./middleware/cors');
